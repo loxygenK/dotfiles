1a7e4b7eb00400d0c1b8705c5f32168e95cfb962
loxygenK <loxygen.k@gmail.com>
2020-12-11 21:27:40 +0900

    Revert "🐠 Fishの環境が整った (#3)"
    This reverts commit 6b197bf98c4deb6953d4d992a9e200a7158d7c63, reversing
    changes made to 81ac7cac23c683385dbeb9163c527837effd3ffa.

---
+++ zshrc.d/005_completion.sh
@@ -0,0 +1,44 @@
+# -----------------------------
+# Completion
+# -----------------------------
+# 自動補完を有効にする
+autoload -Uz compinit ; compinit
+
+# 単語の入力途中でもTab補完を有効化
+setopt complete_in_word
+
+# コマンドミスを修正
+setopt correct
+
+# 補完の選択を楽にする
+zstyle ':completion:*' menu select
+
+# 補完候補をできるだけ詰めて表示する
+setopt list_packed
+
+# 補完候補にファイルの種類も表示する
+#setopt list_types
+
+# 色の設定
+export LSCOLORS=Exfxcxdxbxegedabagacad
+
+# 補完時の色設定
+export LS_COLORS='di=01;34:ln=01;35:so=01;32:ex=01;31:bd=46;34:cd=43;34:su=41;30:sg=46;30:tw=42;30:ow=43;30'
+
+# キャッシュの利用による補完の高速化
+zstyle ':completion::complete:*' use-cache true
+
+# 補完候補に色つける
+autoload -U colors ; colors ; zstyle ':completion:*' list-colors "${LS_COLORS}"
+#zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
+
+# 大文字・小文字を区別しない(大文字を入力した場合は区別する)
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
+
+# manの補完をセクション番号別に表示させる
+zstyle ':completion:*:manuals' separate-sections true
+
+# --prefix=/usr などの = 以降でも補完
+setopt magic_equal_subst
+
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
---
+++ zshrc.d/007_alias.sh
@@ -0,0 +1,57 @@
+# -----------------------------
+# Alias
+# -----------------------------
+# グローバルエイリアス
+alias -g ":l"='| less'
+alias -g ":h"='| head'
+alias -g ":g"='| grep'
+alias -g ":gi"='| grep -ri'
+
+alias -g "w/osu"="WINEPREFIX=$HOME/.wpfx_osu"
+alias -g "w/tetris"="WINEPREFIX=$HOME/.tetris"
+alias -g "w/gen"="WINEPREFIX=$HOME/.wine"
+
+# Make it more sexy(文字通り)
+alias yarn='hentai_yarn && yarn'
+alias -g "yarn aegu"="hentai_yarn"
+
+# Unsexialize
+alias nyarn='"yarn"'
+
+alias vim="nvim"
+alias nv="nvim"
+
+alias lst='ls -ltr --color=auto'
+alias ls='ls --color=auto'
+alias la='ls -la --color=auto'
+alias ll='ls -l --color=auto'
+
+alias al "sl -a"
+
+alias du="du -h"
+alias df="df -Th"
+alias su="su -l"
+
+alias cp='cp -i'
+alias rm='rm -i'
+alias mkdir='mkdir -p'
+alias diff='diff -U1'
+
+alias tma='tmux attach'
+alias tml='tmux list-window'
+
+alias dki="docker run -i -t -P"
+alias dex="docker exec -i -t"
+alias drmf='docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q)'
+
+alias gs="git status"
+alias gb="git branch"
+alias gp="git push origin "
+alias gg="git log --oneline --graph"
+
+alias python="python3"
+alias pip="pip3"
+
+alias bt="blin-tool"
+
+alias export-pdf="wkhtmltopdf --print-media-type --header-right '[date]' --header-spacing 2 --footer-center '[page]/[topage]' --footer-spacing 2 --margin-top 20 --margin-right 20 --margin-left 20 --margin-bottom 20"
