2544d3fa0f39660147b2d3288f47cd0fb1111027
flisan <loxygen.k@gmail.com>
2020-12-11 21:33:04 +0900

    Revert "🐠 Fishの環境が整った (#3)" (#9)
    This reverts commit 6b197bf98c4deb6953d4d992a9e200a7158d7c63, reversing
    changes made to 81ac7cac23c683385dbeb9163c527837effd3ffa.

--- fish/fish.config.d/000_plugins.sh
+++
@@ -1,9 +0,0 @@
-# TODO: Plugin setting for fish.
-
-set NVM_DIR "$HOME/.nvm"
-function nvm
-	bass source $NVM_DIR/nvm.sh --no-use ";" nvm $argv
-end
-
-nvm use default --silent
-status --is-interactive; and source (pyenv init -|psub)
