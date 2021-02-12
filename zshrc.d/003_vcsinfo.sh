9e80459ec31bb7b2eeabfaff7a217b9beefc31ca
flisan <loxygen.k@gmail.com>
2021-02-12 23:23:18 +0900

    💄 Create color schema for light theme (#19)
    * 💄 Create color schema for light theme
    
    * 🔧 Use terminal emulator color to better solarized light

--- zshrc.d/003_vcsinfo.sh
+++ zshrc.d/003_vcsinfo.sh
@@ -1,9 +1,12 @@
-local SEPARATOR="%F{229}†%f"
+source $ZSHRCD_LOCATION/variables/prompt_color.sh
+
+local MAJOR_SEPARATOR="%F{$SEPARATOR_COLOR}†%f"
+local SEPARATOR="%F{$SEPARATOR_COLOR}･%f"
 
 autoload -Uz vcs_info
 setopt prompt_subst
 zstyle ':vcs_info:git:*' check-for-changes true
-zstyle ':vcs_info:git:*' stagedstr " %F{154}✓%f"
-zstyle ':vcs_info:git:*' unstagedstr " %F{217}!%f"
-zstyle ':vcs_info:*' formats " $SEPARATOR %F{069}%b%f%c%u"
-zstyle ':vcs_info:*' actionformats " $SEPARATOR %F{069}%b%f%u %F{245}(now %F{148}%a%F{245})%f"
+zstyle ':vcs_info:git:*' stagedstr " $SEPARATOR %F{$GIT_STAGED}✓ READY%f"
+zstyle ':vcs_info:git:*' unstagedstr " $SEPARATOR %F{$GIT_UNSTAGED}- OUT%f"
+zstyle ':vcs_info:*' formats " $MAJOR_SEPARATOR %F{$GIT_BRANCH}%b%f%c%u"
+zstyle ':vcs_info:*' actionformats " $MAJOR_SEPARATOR %F{$GIT_BRANCH}%b%f%u %F{$SUBTEXT_COLOR}(now %F{$GIT_ACTION}%a%F{$SUBTEXT_COLOR})%f"
--- zshrc.d/011_syntaxhighlight.sh
+++ zshrc.d/011_syntaxhighlight.sh
@@ -1,44 +1,40 @@
+source $ZSHRCD_LOCATION/variables/prompt_color.sh
+
 # †MORE RICH†なハイライト
 ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern)
 
 # よくわからんトークンは灰色+Bold
-ZSH_HIGHLIGHT_STYLES[unknown-token]=fg=246
+ZSH_HIGHLIGHT_STYLES[unknown-token]=fg=$ZSH_UNKNOWN
 
 # ifやforの予約語はいい感じに濁った橙色
-ZSH_HIGHLIGHT_STYLES[reserved-word]=fg=180,bold
+ZSH_HIGHLIGHT_STYLES[reserved-word]=fg=$ZSH_RESERVED,bold
 
 # コマンドは太字に水色、エイリアスは+下線
-ZSH_HIGHLIGHT_STYLES[command]=fg=117,bold
-ZSH_HIGHLIGHT_STYLES[alias]=fg=117,bold,underline
+ZSH_HIGHLIGHT_STYLES[command]=fg=$ZSH_COMMAND,bold
+ZSH_HIGHLIGHT_STYLES[alias]=fg=$ZSH_ALIAS,bold,underline
 
 # ビルトインはなお水色
-ZSH_HIGHLIGHT_STYLES[builtin]=fg=039,bold
+ZSH_HIGHLIGHT_STYLES[builtin]=fg=$ZS_BUILTIN,bold
 
 # 関数はコマンドよりも紫に近い色
-ZSH_HIGHLIGHT_STYLES[function]=fg=105,bold
-
-# コマンドの区切りは薄い水色
-ZSH_HIGHLIGHT_STYLES[commandseparator]=fg=159,bold
+ZSH_HIGHLIGHT_STYLES[function]=fg=$ZSH_FUNCTION,bold
 
 # パスは薄めの緑
-ZSH_HIGHLIGHT_STYLES[path]=fg=157
-
-# 区切りはもうちょい濃めの緑(動いてなさそう)
-ZSH_HIGHLIGHT_STYLES[path_pathseparator]=fg=34
+ZSH_HIGHLIGHT_STYLES[path]=fg=$ZSH_PATH
 
 # ファイル複数指定時の「*」とかは絶望的に見づらいので紫
-ZSH_HIGHLIGHT_STYLES[globbing]=fg=177
+ZSH_HIGHLIGHT_STYLES[globbing]=fg=$ZSH_GLOB
 
 # 他はちょっと灰色にする(お目目に優しくなったらいいな)
-ZSH_HIGHLIGHT_STYLES[default]=fg=250
+ZSH_HIGHLIGHT_STYLES[default]=fg=$ZSH_DEFAULT
 
 # オプションは黄色
-ZSH_HIGHLIGHT_STYLES[single-hyphen-option]=fg=227
-ZSH_HIGHLIGHT_STYLES[double-hyphen-option]=fg=222
+ZSH_HIGHLIGHT_STYLES[single-hyphen-option]=fg=$ZSH_OPTION
+ZSH_HIGHLIGHT_STYLES[double-hyphen-option]=fg=$ZSH_OPTION_LONG
 
 # 文字列はオレンジ
-ZSH_HIGHLIGHT_STYLES[single-quoted-argument]=fg=208
-ZSH_HIGHLIGHT_STYLES[double-quoted-argument]=fg=208
+ZSH_HIGHLIGHT_STYLES[single-quoted-argument]=fg=$ZSH_STRING
+ZSH_HIGHLIGHT_STYLES[double-quoted-argument]=fg=$ZSH_STRING
 
 # コマンド展開するあのようわからんやつ(`)は濁った橙色
-ZSH_HIGHLIGHT_STYLES[back-quoted-argument-delimiter]=fg=136
+ZSH_HIGHLIGHT_STYLES[back-quoted-argument-delimiter]=fg=$ZSH_CMDEXPAND
