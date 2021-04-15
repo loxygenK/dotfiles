e34d32d112b09c75e51cb391f3205052d1476f94
loxygenk <me@loxygen.dev>
2021-04-15 18:38:15 +0900

    🚚 Improve the configuration order of paths

--- shell/zshrc.d/010_paths.sh
+++ shell/zshrc.d/010_paths.sh
@@ -1,14 +1,4 @@
 # -----------------------------
-# PATH
-# -----------------------------
-case "${OSTYPE}" in
-  darwin*)
-    export PATH=/opt/local/bin:/opt/local/sbin:$PATH
-    export MANPATH=/opt/local/share/man:/opt/local/man:$MANPATH
-  ;;
-esac
-
-# -----------------------------
 # Python
 # -----------------------------
 export PYENV_ROOT="$HOME/.pyenv"
@@ -19,13 +9,13 @@ alias pipallupgrade="pip freeze --local | grep -v '^\-e' | cut -d = -f 1  | xarg
 # -----------------------------
 # Golang
 # -----------------------------
-if which go > /dev/null 2>&1  ; then
-  export CGO_ENABLED=1
-  export GOPATH=$HOME/dev/go
-  export PATH=$PATH:$(go env GOROOT)/bin:$GOPATH/bin
-fi
 
-# Android (especially react-native)
+######################
+# PATH configuration #
+######################
+
+# No dependencies
+
 export ANDROID_HOME=$HOME/Android/Sdk
 export PATH=$PATH:$ANDROID_HOME/emulator
 export PATH=$PATH:$ANDROID_HOME/tools
@@ -35,36 +25,56 @@ export PATH=$PATH:$ANDROID_HOME/platform-tools
 export PATH=$PATH:$HOME/.local/bin
 export PATH=$PATH:$HOME/.selfmade_nice_programs
 
-eval "$(pyenv init -)"
-eval "$(pyenv virtualenv-init -)"
-
-# Flutterのために
 export PATH="$PATH:$HOME/.flutter/bin"
 
-# Dart SDKのために
 export PATH="$PATH:/usr/lib/dart/bin"
 export PATH="~/.pub-cache/bin:$PATH"
 
 export PATH="$PATH:$HOME/.blin-tool"
 
-# Rustのために
 export PATH="$HOME/.cargo/bin:$PATH"
 
-# node
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
 export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
-source /usr/share/nvm/init-nvm.sh
+
+export PATH="$PATH:$HOME/.misc"
+export PATH="$PATH:$HOME/.executable"
+
+# Depending
+
+if which go > /dev/null 2>&1  ; then
+  export CGO_ENABLED=1
+  export GOPATH=$HOME/dev/go
+  export PATH=$PATH:$(go env GOROOT)/bin:$GOPATH/bin
+fi
+
+# Other environment variables
+
+export NVM_DIR="$HOME/.nvm"
+export GPG_TTY=$(tty)
+export SDKMAN_DIR="/home/flisan/.sdkman"
+
+# scripts
+
+if [ "$1" = "no-script" ]; then
+  echo "Script suppressed; Exiting"
+  return
+fi
+
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
 
 eval "$(rbenv init -)"
 
-export GPG_TTY=$(tty)
+source /usr/share/nvm/init-nvm.sh
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-export PATH="$PATH:`"yarn" global bin`"
-
-#THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
-export SDKMAN_DIR="/home/flisan/.sdkman"
 [[ -s "/home/flisan/.sdkman/bin/sdkman-init.sh" ]] && source "/home/flisan/.sdkman/bin/sdkman-init.sh"
+
+# Deferred
+
+if which yarn > /dev/null 2>&1; then
+  export PATH="$PATH:`"yarn" global bin`"
+fi
+
 
