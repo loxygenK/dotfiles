ee525b99e681bc14fab770810a35fdb7d21d7128
loxygenk <me@loxygen.dev>
2021-04-15 18:35:41 +0900

    🚚 Change the loading order of plugins

--- shell/zshrc.d/0035_plugins.sh
+++ shell/zshrc.d/0035_plugins.sh
@@ -11,11 +11,24 @@ source ~/.zplug/init.zsh
 
 # プラグインList
 # zplug "ユーザー名/リポジトリ名", タグ
+
+# Completion
 zplug "zsh-users/zsh-completions"
+
+# Zsh Auto Suggestion
 zplug "zsh-users/zsh-autosuggestions"
+
+# Syntax Highlighting
 zplug "zsh-users/zsh-syntax-highlighting", defer:2
+
+# Add Visual mode
 zplug "b4b4r07/zsh-vimode-visual", defer:3
+
+# Enhanced cD
 zplug "b4b4r07/enhancd", use:init.sh
+
+# Abbreviation
+zplug "olets/zsh-abbr"
 #zplug "junegunn/fzf-bin", as:command, from:gh-r, file:fzf
 
 # インストールしていないプラグインをインストール
