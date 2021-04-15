e7e9389119d73e49bdb4be744d43bea7b6e6f4f4
flisan <loxygen.k@gmail.com>
2021-04-15 19:15:32 +0900

    🚚 Organized local change (#25)
    * 🚚 Change the loading order of plugins
    
    * 🚚 Improve the configuration order of paths
    
    * ♿️ Use "abbreviation" instead of "alias"
    
    * ✨ Load fzf.zsh
    
    * 💬 Use emoji to indicate errors
    
    * 🎨 Change color schema to One light
    
    * ⚡️ Improve behaivor of dein.vim
    
    * 🍱 Add plugins for neovim
    
    * 🙈 Ignore local-depending file. (.netrwhist)
    
    * ✨ Improve appearance and behaivor of conky
    
    * ✨ Improved appearance and behaivor of dunst
    
    * 🎨 Change color of status bar

--- shell/zshrc.d/007_alias.sh
+++ shell/zshrc.d/007_alias.sh
@@ -18,13 +18,12 @@ alias -g "yarn aegu"="hentai_yarn"
 # Unsexialize
 alias nyarn='"yarn"'
 
+alias cat="bat --theme='Solarized (light)' --style='numbers,grid'"
+
 alias vim="nvim"
 alias nv="nvim"
 
-alias lst='ls -ltr --color=auto'
 alias ls='ls --color=auto'
-alias la='ls -la --color=auto'
-alias ll='ls -l --color=auto'
 
 alias al "sl -a"
 
@@ -37,21 +36,6 @@ alias rm='rm -i'
 alias mkdir='mkdir -p'
 alias diff='diff -U1'
 
-alias tma='tmux attach'
-alias tml='tmux list-window'
-
-alias dki="docker run -i -t -P"
-alias dex="docker exec -i -t"
-alias drmf='docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q)'
-
-alias gs="git status -bs"
-alias gb="git branch"
-alias gd="git diff"
-alias gds="git diff --staged"
-alias gc="git checkout"
-alias gp="git push origin "
-alias gg="git log --oneline --graph"
-
 alias python="python3"
 alias pip="pip3"
 
@@ -62,3 +46,29 @@ alias bt="blin-tool"
 alias export-pdf="wkhtmltopdf --print-media-type --header-right '[date]' --header-spacing 2 --footer-center '[page]/[topage]' --footer-spacing 2 --margin-top 20 --margin-right 20 --margin-left 20 --margin-bottom 20"
 
 alias gmc="gitmoji -c"
+
+# --- Abbr. ---
+alias abbr="abbr --session --quiet --force"
+
+abbr c='cd'
+
+abbr lst='ls -ltr'
+abbr la='ls -la'
+abbr ll='ls -l'
+
+abbr tma='tmux attach'
+abbr tml='tmux list-window'
+
+abbr dki="docker run -i -t -P"
+abbr dex="docker exec -i -t"
+abbr drmf='docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q)'
+
+abbr gs="git status -bs"
+abbr gb="git branch"
+abbr gd="git diff"
+abbr gds="git diff --staged"
+abbr gc="git checkout"
+abbr gp="git push origin "
+abbr gg="git log --oneline --graph"
+
+unalias abbr
