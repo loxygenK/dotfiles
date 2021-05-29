26cfbfdbdff466d8a237a7d84593a7001dda80c8
flisan <me@loxygen.dev>
2021-05-29 23:28:27 +0900

    ✨ Adjust syntax highlight and add some features (#35)
    * 🔥 Remove dark theme support for syntax highlighting
    
    * 🔥 Remove unused files
    
    * 🚚 Fix numbering of the zshrc.d shell script
    
    * 🎨 Adjust color of the syntax highlignting
    
    * 🎨 Set up SPROMPT
    
    * ✨ Add No EOF prompt
    
    * ✨ Print the status code and its descriptionc

--- shell/zshrc.d/011_syntaxhighlight.sh
+++ shell/zshrc.d/011_syntaxhighlight.sh
@@ -1,40 +1,34 @@
-source $ZSHRCD_LOCATION/_variables/prompt_color.sh
-
-# †MORE RICH†なハイライト
 ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern)
 
-# よくわからんトークンは灰色+Bold
-ZSH_HIGHLIGHT_STYLES[unknown-token]=fg=$ZSH_UNKNOWN
+# --- Values
+ZSH_HIGHLIGHT_STYLES[default]=fg=none
+ZSH_HIGHLIGHT_STYLES[unknown-token]=fg=124,underline
+ZSH_HIGHLIGHT_STYLES[reserved-word]=fg=21
 
-# ifやforの予約語はいい感じに濁った橙色
-ZSH_HIGHLIGHT_STYLES[reserved-word]=fg=$ZSH_RESERVED,bold
+# --- Commands
+ZSH_HIGHLIGHT_STYLES[command]=fg=33
+ZSH_HIGHLIGHT_STYLES[precommand]=fg=33
+ZSH_HIGHLIGHT_STYLES[hashed-command]=fg=33,bold
 
-# コマンドは太字に水色、エイリアスは+下線
-ZSH_HIGHLIGHT_STYLES[command]=fg=$ZSH_COMMAND,bold
-ZSH_HIGHLIGHT_STYLES[alias]=fg=$ZSH_ALIAS,bold,underline
+# --- Command-ish thingies
+ZSH_HIGHLIGHT_STYLES[alias]=fg=31
+ZSH_HIGHLIGHT_STYLES[builtin]=fg=27
+ZSH_HIGHLIGHT_STYLES[function]=fg=29
+ZSH_HIGHLIGHT_STYLES[commandseparator]=fg=69
+ZSH_HIGHLIGHT_STYLES[assign]=none
+ZSH_HIGHLIGHT_STYLES[redirect]=fg=22
 
-# ビルトインはなお水色
-ZSH_HIGHLIGHT_STYLES[builtin]=fg=$ZS_BUILTIN,bold
+# --- Paths
+ZSH_HIGHLIGHT_STYLES[path]=fg=30
+ZSH_HIGHLIGHT_STYLES[path_prefix]=fg=30
+ZSH_HIGHLIGHT_STYLES[path_approx]=fg=30
+ZSH_HIGHLIGHT_STYLES[globbing]=fg=36
 
-# 関数はコマンドよりも紫に近い色
-ZSH_HIGHLIGHT_STYLES[function]=fg=$ZSH_FUNCTION,bold
-
-# パスは薄めの緑
-ZSH_HIGHLIGHT_STYLES[path]=fg=$ZSH_PATH
-
-# ファイル複数指定時の「*」とかは絶望的に見づらいので紫
-ZSH_HIGHLIGHT_STYLES[globbing]=fg=$ZSH_GLOB
-
-# 他はちょっと灰色にする(お目目に優しくなったらいいな)
-ZSH_HIGHLIGHT_STYLES[default]=fg=$ZSH_DEFAULT
-
-# オプションは黄色
-ZSH_HIGHLIGHT_STYLES[single-hyphen-option]=fg=$ZSH_OPTION
-ZSH_HIGHLIGHT_STYLES[double-hyphen-option]=fg=$ZSH_OPTION_LONG
-
-# 文字列はオレンジ
-ZSH_HIGHLIGHT_STYLES[single-quoted-argument]=fg=$ZSH_STRING
-ZSH_HIGHLIGHT_STYLES[double-quoted-argument]=fg=$ZSH_STRING
-
-# コマンド展開するあのようわからんやつ(`)は濁った橙色
-ZSH_HIGHLIGHT_STYLES[back-quoted-argument-delimiter]=fg=$ZSH_CMDEXPAND
+# --- Values
+ZSH_HIGHLIGHT_STYLES[single-hyphen-option]=fg=24
+ZSH_HIGHLIGHT_STYLES[double-hyphen-option]=fg=24
+ZSH_HIGHLIGHT_STYLES[back-quoted-argument]=fg=31
+ZSH_HIGHLIGHT_STYLES[single-quoted-argument]=fg=36
+ZSH_HIGHLIGHT_STYLES[double-quoted-argument]=fg=36
+ZSH_HIGHLIGHT_STYLES[dollar-double-quoted-argument]=fg=31
+ZSH_HIGHLIGHT_STYLES[back-double-quoted-argument]=fg=31
