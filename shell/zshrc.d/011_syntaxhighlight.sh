2ee9620334c585caecf3a3e200deefccd618f44d
loxygenk <me@loxygen.dev>
2021-05-29 22:14:30 +0900

    🔥 Remove dark theme support for syntax highlighting

--- shell/zshrc.d/011_syntaxhighlight.sh
+++ shell/zshrc.d/011_syntaxhighlight.sh
@@ -1,40 +1,38 @@
-source $ZSHRCD_LOCATION/_variables/prompt_color.sh
-
 # †MORE RICH†なハイライト
 ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern)
 
 # よくわからんトークンは灰色+Bold
-ZSH_HIGHLIGHT_STYLES[unknown-token]=fg=$ZSH_UNKNOWN
+ZSH_HIGHLIGHT_STYLES[unknown-token]=fg=244
 
 # ifやforの予約語はいい感じに濁った橙色
-ZSH_HIGHLIGHT_STYLES[reserved-word]=fg=$ZSH_RESERVED,bold
+ZSH_HIGHLIGHT_STYLES[reserved-word]=fg=166,bold
 
 # コマンドは太字に水色、エイリアスは+下線
-ZSH_HIGHLIGHT_STYLES[command]=fg=$ZSH_COMMAND,bold
-ZSH_HIGHLIGHT_STYLES[alias]=fg=$ZSH_ALIAS,bold,underline
+ZSH_HIGHLIGHT_STYLES[command]=fg=39,bold
+ZSH_HIGHLIGHT_STYLES[alias]=fg=39,bold,underline
 
 # ビルトインはなお水色
-ZSH_HIGHLIGHT_STYLES[builtin]=fg=$ZS_BUILTIN,bold
+ZSH_HIGHLIGHT_STYLES[builtin]=fg=32,bold
 
 # 関数はコマンドよりも紫に近い色
-ZSH_HIGHLIGHT_STYLES[function]=fg=$ZSH_FUNCTION,bold
+ZSH_HIGHLIGHT_STYLES[function]=fg=62,bold
 
 # パスは薄めの緑
-ZSH_HIGHLIGHT_STYLES[path]=fg=$ZSH_PATH
+ZSH_HIGHLIGHT_STYLES[path]=fg=32
 
 # ファイル複数指定時の「*」とかは絶望的に見づらいので紫
-ZSH_HIGHLIGHT_STYLES[globbing]=fg=$ZSH_GLOB
+ZSH_HIGHLIGHT_STYLES[globbing]=fg=105
 
 # 他はちょっと灰色にする(お目目に優しくなったらいいな)
-ZSH_HIGHLIGHT_STYLES[default]=fg=$ZSH_DEFAULT
+ZSH_HIGHLIGHT_STYLES[default]=fg=240
 
 # オプションは黄色
-ZSH_HIGHLIGHT_STYLES[single-hyphen-option]=fg=$ZSH_OPTION
-ZSH_HIGHLIGHT_STYLES[double-hyphen-option]=fg=$ZSH_OPTION_LONG
+ZSH_HIGHLIGHT_STYLES[single-hyphen-option]=fg=70
+ZSH_HIGHLIGHT_STYLES[double-hyphen-option]=fg=70
 
 # 文字列はオレンジ
-ZSH_HIGHLIGHT_STYLES[single-quoted-argument]=fg=$ZSH_STRING
-ZSH_HIGHLIGHT_STYLES[double-quoted-argument]=fg=$ZSH_STRING
+ZSH_HIGHLIGHT_STYLES[single-quoted-argument]=fg=136
+ZSH_HIGHLIGHT_STYLES[double-quoted-argument]=fg=136
 
 # コマンド展開するあのようわからんやつ(`)は濁った橙色
-ZSH_HIGHLIGHT_STYLES[back-quoted-argument-delimiter]=fg=$ZSH_CMDEXPAND
+ZSH_HIGHLIGHT_STYLES[back-quoted-argument-delimiter]=fg=100
--- shell/zshrc.d/_variables/prompt_color.sh
+++ shell/zshrc.d/_variables/prompt_color.sh
@@ -22,19 +22,3 @@ __light && GIT_STAGED=70       || GIT_STAGED=154
 __light && GIT_UNSTAGED=166    || GIT_STAGED=217
 __light && GIT_BRANCH=20       || GIT_BRANCH=69
 __light && GIT_ACTION=70       || GIT_ACTION=148
-
-# --- Zsh detailed syntax highlight setting ---
-__light && ZSH_UNKNOWN=244     || ZSH_UNKNOWN=246
-__light && ZSH_RESERVED=166    || ZSH_RESERVED=180
-__light && ZSH_COMMAND=39      || ZSH_COMMAND=117
-__light && ZSH_ALIAS=39        || ZSH_ALIAS=117
-__light && ZSH_BUILTIN=32      || ZSH_BUILTIN=39
-__light && ZSH_FUNCTION=62     || ZSH_FUNCTION=105
-__light && ZSH_SEPARATOR=45    || ZSH_SEPARATOR=159
-__light && ZSH_PATH=34         || ZSH_PATH=157
-__light && ZSH_GLOB=105        || ZSH_GLOB=177
-__light && ZSH_OPTION=70       || ZSH_OPTION=227
-__light && ZSH_OPTION_LONG=70  || ZSH_OPTION_LONG=222
-__light && ZSH_STRING=136      || ZSH_STRING=208
-__light && ZSH_CMDEXPAND=100   || ZSH_CMDEXPAND=136
-__light && ZSH_DEFAULT=240     || ZSH_DEFAULT=250
